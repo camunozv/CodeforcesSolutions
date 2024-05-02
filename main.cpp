cc917055f0e60faac81e820a257bcc687f30b35d
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2024-05-01 22:15:24 -0500

    Code template created

--- main.cpp
+++ main.cpp
@@ -7,6 +7,12 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    cout << "Hello world \n";
+    // Exercise difficulty:
+    // Exercise name:
+    // Link to the exercise:
+
+    // Solution:
+    // Code here...
+
     return 0;
 }
