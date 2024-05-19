7374512b04e5921420fa6ef7a61913c795297052
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2024-05-19 16:07:02 -0500

    Elephant

--- main.cpp
+++ main.cpp
@@ -7,12 +7,22 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    // Exercise difficulty:
-    // Exercise name:
-    // Link to the exercise:
+    // Exercise difficulty: 800
+    // Exercise name: Elephant
+    // Link to the exercise: https://codeforces.com/problemset/problem/617/A
 
     // Solution:
-    // Code here...
+    int house = 0;
+
+    cin >> house;
+
+    int steps = house / 5;
+    if (house % 5 == 0){
+        cout << steps << "\n";
+    } else {
+        steps += 1;
+        cout << steps << "\n";
+    }
 
     return 0;
 }
