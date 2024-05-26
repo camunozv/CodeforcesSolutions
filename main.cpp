205304291ee87e2cd1d13e003572b262597fb0af
CarlosIvanMunozVillazon <93873199+CarlosIvanMunozVillazon@users.noreply.github.com>
2024-05-26 15:45:24 -0500

    Min Max swap solved; ohne die Lösung zu sehen

--- main.cpp
+++ main.cpp
@@ -34,11 +34,6 @@ int main() {
             cin >> b[j];
         }
 
-        sort(a.begin(), a.end());
-        sort(b.begin(), b.end());
-
-        // function that solves the problem goes here.
-
         cout << min_max_swap(a, b, n) << "\n";
         i++;
     }
@@ -48,46 +43,28 @@ int main() {
 
 int min_max_swap(vector<int> &a, vector<int> &b, int n) {
 
-    int max_a = a[n - 1];
-    int max_b = b[n - 1];
     int swap = 0;
-    int prod = 0;
-    if (max_a > max_b) {
-        for (int i = 0; i < n; i++) {
-            if (b[n - 1 - i] > a[i]) {
-                swap = a[i];
-                a[i] = b[n - 1 - i];
-                b[n - 1 - i] = swap;
-            }
+
+    for (int i = 0; i < n; i++) {
+        if (b[i] > a[i]) {
+            swap = a[i];
+            a[i] = b[i];
+            b[i] = swap;
         }
-
-        swap = b[0];
-        for (int i = 1; i < n; i++) {
-            if (b[i] > swap) {
-                swap = b[i];
-            }
-        }
-
-        prod = max_a;
-
-    } else {
-        for (int i = 0; i < n; i++) {
-            if (b[i] < a[n - 1 - i]) {
-                swap = b[i];
-                b[i] = a[n - 1 - i];
-                a[n - 1 - i] = swap;
-            }
-        }
-
-        swap = a[0];
-        for (int i = 1; i < n; i++) {
-            if (a[i] > swap) {
-                swap = a[i];
-            }
-        }
-
-        prod = max_b;
     }
 
-    return swap * prod;
+    int max_a = a[0];
+    int max_b = b[0];
+
+    for (int i = 1; i < n; i++) {
+
+        if (a[i] > max_a) {
+            max_a = a[i];
+        }
+        if (b[i] > max_b) {
+            max_b = b[i];
+        }
+    }
+
+    return max_a * max_b;
 }
