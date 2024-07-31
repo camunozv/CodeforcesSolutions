5659e4e3de40cb4dd83e329dfcf1478905125d08
camunozv <93873199+camunozv@users.noreply.github.com>
2024-07-31 08:05:41 -0500

    Strong Password solved

--- main.cpp
+++ main.cpp
@@ -2,67 +2,96 @@
 
 using namespace std;
 
-int euclides_algorithm(int a, int b);
-
-int solution(int x);
+void solve_string(string chain);
 
 int main() {
 
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    // Exercise difficulty: 800
-    // Exercise name: A. Maximize?
-    // Link to the exercise: https://codeforces.com/problemset/problem/1968/A
+    // Exercise difficulty: --
+    // Exercise name: Strong Password
+    // Link to the exercise: https://codeforces.com/contest/1997/problem/A
 
     // Solution:
 
     int t = 0;
-
     cin >> t;
 
-    vector<int> test_cases(t);
+    int i = 0;
 
-    for (int i = 0; i < t; i++) {
-        cin >> test_cases[i];
+    while (i < t) {
+
+        string case_test;
+        cin >> case_test;
+        solve_string(case_test);
+        i++;
     }
 
-    for (int i = 0; i < t; i++) {
-        cout << solution(test_cases[i]) << "\n";
-    }
 
     return 0;
 }
 
-int euclides_algorithm(int a, int b) {
+void solve_string(string chain) {
 
-    int big = a > b ? a : b;
-    int small = a >= b ? b : a;
+    vector<char> alphabet = {
+            'a', 'b', 'c', 'd',
+            'e', 'f', 'g', 'h',
+            'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x',
+            'y', 'z'
+    };
 
-    int mod = big % small;
-    int mcd = small;
+    int n = chain.size();
+    int i = 0;
+    bool equality = false;
 
-    while (mod != 0) {
-        big = small;
-        small = mod;
-
-        mod = big % small;
-        mcd = small;
-    }
-
-    return mcd;
-}
-
-int solution(int x) {
-
-    int max = 0;
-    int possible = 0;
-    for (int i = 1; i < x; i++) {
-        possible = euclides_algorithm(i, x) + i;
-        if (possible > max) {
-            max = possible;
+    while (i < n - 1 && !equality) {
+        if (chain[i] == chain[i + 1]) {
+            equality = true;
         }
+        i++;
     }
-    max -= 1;
-    return max;
+
+    i--;
+
+    if (equality) {
+        vector<char> chain_2;
+        for (int j = 0; j <= i; j++) {
+            chain_2.push_back(chain[j]);
+        }
+        int k = 0;
+        while (chain_2[i] == alphabet[k]) {
+            k++;
+        }
+
+        chain_2.push_back(alphabet[k]);
+
+        for (int r = i + 1; r < n; r++) {
+            chain_2.push_back(chain[r]);
+        }
+
+        for (auto letter: chain_2) {
+            cout << letter;
+        }
+
+        cout << "\n";
+
+    } else {
+        int k = 0;
+        while (chain[n - 1] == alphabet[k]) {
+            k++;
+        }
+
+        chain.push_back(alphabet[k]);
+
+        for (auto letter: chain) {
+            cout << letter;
+        }
+
+        cout << "\n";
+    }
+
 }
