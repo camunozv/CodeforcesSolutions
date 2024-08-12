18bad3c079010632e5a233d4143614591a618a4d
camunozv <93873199+camunozv@users.noreply.github.com>
2024-08-11 19:18:35 -0500

    a+b again solved

--- main.cpp
+++ main.cpp
@@ -2,47 +2,33 @@
 
 using namespace std;
 
-int solve(string answers);
-
-int main() {
+int solve(int number);
+int main () {
 
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int t = 0;
-    int n = 0;
-    int i = 0;
+    int test_cases = 0;
 
-    cin >> t;
+    cin >> test_cases;
 
-    while (i < t) {
-        cin >> n;
-        string answers;
-        cin >> answers;
+    while(test_cases > 0) {
 
-        cout << solve(answers) << "\n";
-        i++;
+        int number = 0;
+        cin >> number;
+
+        cout << solve(number) << "\n";
+
+        test_cases--;
     }
 
     return 0;
 }
 
-int solve(string answers) {
+int solve(int number) {
 
-    int result = 0;
-    int n = answers.size();
-    map<char, int> my_map;
-    my_map['A'] = n / 4;
-    my_map['B'] = n / 4;
-    my_map['C'] = n / 4;
-    my_map['D'] = n / 4;
+    int a = number / 10;
+    int b = number % 10;
 
-    for (int i = 0; i < n; i++) {
-        if (answers[i] != '?' && my_map[answers[i]] > 0) {
-            my_map[answers[i]] -= 1;
-            result++;
-        }
-    }
-
-    return result;
+    return a + b;
 }
