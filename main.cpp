2da01a9d339cf91d2ab67fb7b517cfcea5649865
camunozv <93873199+camunozv@users.noreply.github.com>
2024-10-14 10:09:59 -0500

    First Div 2 problem solved

--- main.cpp
+++ main.cpp
@@ -2,33 +2,37 @@
 
 using namespace std;
 
-int solve(int number);
-int main () {
+int solve(string s, string t) {
 
+    int i = 0, j = 0, k = 0;
+    unsigned int n = s.size(), m = t.size();
+    while (i < n && j < m && s[i] == t[i]) {
+        i++;
+        j++;
+        k++;
+    }
+    if (k > 0) {
+        k++;
+    }
+
+    k += n - i;
+    k += m - j;
+
+    return k;
+}
+
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int test_cases = 0;
-
-    cin >> test_cases;
-
-    while(test_cases > 0) {
-
-        int number = 0;
-        cin >> number;
-
-        cout << solve(number) << "\n";
-
-        test_cases--;
+    int q = 0, i = 0;
+    cin >> q;
+    string s, t;
+    while (i < q) {
+        cin >> s >> t;
+        cout << solve(s, t) << "\n";
+        i++;
     }
 
     return 0;
-}
-
-int solve(int number) {
-
-    int a = number / 10;
-    int b = number % 10;
-
-    return a + b;
 }
