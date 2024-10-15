fc36cf6575ba3a265002aa86271005ff1a9f04b0
camunozv <93873199+camunozv@users.noreply.github.com>
2024-10-14 22:07:52 -0500

    Robin helps solved

--- main.cpp
+++ main.cpp
@@ -2,35 +2,40 @@
 
 using namespace std;
 
-int solve(string s, string t) {
-
-    int i = 0, j = 0, k = 0;
-    unsigned int n = s.size(), m = t.size();
-    while (i < n && j < m && s[i] == t[i]) {
-        i++;
-        j++;
-        k++;
+int solve(int k, vector<int> arr) {
+    int c = 0, r = 0;
+    for (auto n: arr) {
+        if (n == 0 && r > 0) {
+            r--;
+            c++;
+        } else if (n >= k) {
+            r += n;
+            n = 0;
+        }
     }
-    if (k > 0) {
-        k++;
-    }
 
-    k += n - i;
-    k += m - j;
-
-    return k;
+    return c;
 }
 
 int main() {
+
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int q = 0, i = 0;
-    cin >> q;
-    string s, t;
-    while (i < q) {
-        cin >> s >> t;
-        cout << solve(s, t) << "\n";
+    // Exercise difficulty: 800
+    // Exercise name: Robin Helps
+    // Link to the exercise: https://codeforces.com/contest/2014/problem/A
+    // Solution:
+    int t = 0;
+    cin >> t;
+    int i = 0, n = 0, k = 0;
+    while (i < t) {
+        cin >> n >> k;
+        vector<int> people(n);
+        for (int r = 0; r < n; r++) {
+            cin >> people[r];
+        }
+        cout << solve(k, people) << "\n";
         i++;
     }
 
