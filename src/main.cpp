7b6b13c35d427dc785b7cd28f6d45fe9c4677253
camunozv <93873199+camunozv@users.noreply.github.com>
2025-04-25 13:18:18 -0500

    Added notes about bitmasks

--- src/main.cpp
+++ src/main.cpp
@@ -2,57 +2,36 @@
 
 using namespace std;
 
-int solve(int a, int b)
-{
-    int ret = 0;
-    if (a > b) {
-        ret = 1;
-    } else if (a < b){
-        ret = -1;
-    } 
-    return ret;
-}
-
 int main()
 {
 
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    // Exercise difficulty: 1000
-    // Exercise name: Card Game
-    // Link to the exercise: https://codeforces.com/problemset/problem/1999/B
+    // Exercise difficulty: N/A
+    // Exercise name: Dr.TC
+    // Link to the exercise: 
     // Solution:
-    int t = 0;
 
-    cin >> t;
+    // Im testing a little bit the use of bitmasks. Integers can store up to 32
+    // bits, so we can manipulate them.
+    
+    // << -> this operand is truly a bit shift.
 
-    int a, b, c, d;
+    // So here what we are doing is shifting the number one by 0 positions, remains equal
+    // the second one we are shifting 0 by 4 positions which make it remain equal.
+    // so we can multiply by the power of two. Hence we are applying bitshift.
+    
+    // (1 << 0) -> bit 0 is now ON = 1
+    // (0 << 4) -> bit 1 is now OFF = 0
 
-    int k = 0;
+    int k = (1 << 0) | (1 << 1);
 
-    int ans = 0;
-    while (k < t)
-    {
-        cin >> a >> b >> c >> d;
-        
-        // Bajo este esquema de solución podemos contar las respuestas, sin necesidad de
-        // pensar en todos los casos.
+    cout << k << "\n";
 
-        // Es válido pensar en que alguien gana si hay 2 números iguales, por que nadie gana
-        // en esa ronda, mientras que en la otra, seguramente alguien ganará (2 números diferentes)
-        if (solve(a, c) + solve(b, d) > 0) {
-            ans += 2;
-        }
-
-        if (solve(a, d) + solve(b, c) > 0) {
-            ans += 2;
-        }
-
-        cout << ans << "\n";
-        ans = 0;
-        k++;
-    }
+    k = (0 << 1);
+    
+    cout << k << "\n";
 
     return 0;
 }
