7ffd417136d965953d4fbfa10b88f8e3768be1c9
camunozv <93873199+camunozv@users.noreply.github.com>
2025-07-30 11:00:00 -0500

    Problem solved

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,31 @@
 
 using namespace std;
 
+void multiply_vector(vector<int> &v, int i, int n)
+{
+    for (int j = i; j >= 0; j--)
+    {
+        v[j] *= 2;
+    }
+}
 
+int solve(vector<int> &v, int c, int n)
+{
 
+    int coin_counter = n;
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if (v[i] <= c)
+        {
+            coin_counter--;
+            multiply_vector(v, i - 1, n);
+        }
+        
+    }
+
+    return coin_counter;
+}
 
 int main()
 {
@@ -17,6 +40,7 @@ int main()
     // Solution:
 
     int t = 0;
+
     cin >> t;
 
     int n = 0;
@@ -35,6 +59,8 @@ int main()
         }
 
         sort(v.begin(), v.end());
+
+        cout << solve(v, c, n) << "\n";
 
         t--;
     }
